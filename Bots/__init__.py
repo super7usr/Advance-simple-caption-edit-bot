7c4c5ac9a43ab60cf5912c2ca88e21720d934a8e
super7usr <glenna.lakin@blenched.com>
2025-04-25 18:19:11 +0100

    Add files via upload

---
+++ Bots/__init__.py
@@ -0,0 +1,11 @@
+"""
+Telegram Bot package for advanced caption editing and management.
+This package contains all the bot-specific functionality.
+"""
+
+# Make modules available for import
+from . import Caption
+from . import Force_Sub
+from . import database
+from . import task_processor
+from . import web_support
